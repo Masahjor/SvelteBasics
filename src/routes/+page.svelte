5a4f82a6f1cedf4f5520767eca55a73b1346efc8
Masahjor <madssahl@outlook.dk>
2025-04-23 11:49:49 +0200

    Added fake_name as let

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
     import Header from './Header.svelte'
+    let name = $state('Braingreen');
+    let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+    let full_name = $derived(name + " " + 'Grønsen')
+
+    // function toggle() {
+    //     status = status === "OPEN" ? "CLOSED" : "OPEN";
+    // }
+    function onclick() {
+        status = status === "OPEN" ? "CLOSED" : "OPEN";
+    }
 </script>
 
-<Header />
+<!-- this is a prop. examples: prop={prop_value}, name={name} -->
+<Header {name} />
+
+<h2>{full_name}</h2>
+
+<input type="text" bind:value={name} />
+
+<p>The store is now {status}</p>
+<!-- <button {onclick}>Toggle Status</button> -->
+<button
+    onclick={() => {
+        status = status === "OPEN" ? "CLOSED" : "OPEN";
+    }}>Toggle Status</button
+>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,32 @@
 <!-- this is a comment -->
- 
+
 <!-- Script tag up top -->
 <script lang="ts">
+
+    //* this is a prop
+    let { 
+        name, fake_name = 'Purple' 
+    } : {
+        name: string;
+        fake_name?: string;
+    } = $props();
+    //* $ i syntaxen hedder Runes, DOLLARTEGN -> HVAD DU BRUGER -> FUNKTION
+    //* man kan fx. skabe props med $props, $state til at skabe state. Man kan også bruge derive og effect og meget mere.
+
 </script>
 
 <!-- HTML in the middle -->
-<h1>Hello from Header</h1>
+<h1>Hello {name}, this is Header</h1>
+<h2 class="name_title">{name.replaceAll("r", "w")}</h2>
+<h2 class="fake_name_title">is not {fake_name}</h2>
 
 <!-- CSS at the bottom -->
 <style>
+    .name_title {
+        color: green;
+    }
 
+    .fake_name_title {
+        color: purple;
+    }
 </style>
